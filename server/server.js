417bc83472337ed07ed9fe6e5fa6d1fc66dd67ab
lvlashnin <lvlashnin@gmail.com>
2025-10-24 10:39:41 +0300

    Bind server to 0.0.0.0 for Render

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-const port = process.env.PORT || 10000;
-
-server.listen(port, "0.0.0.0", () => {
-  console.log(`✅ JSON Server is running on port ${port}`);
-});
-
 server.use(middlewares);
 server.use(router);
+
+const PORT = process.env.PORT || 10000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ JSON Server is running on port ${PORT}`);
+});
